19564495ee48bab62a22519c3d44e67a5f35f9c4
Roland Jansen <Roland.Jansen@bitbw.bwl.de>
2021-09-02 16:45:04 +0200

    TryParse Extension for Enum

--- LaYumba.Functional/Enum.cs
+++ LaYumba.Functional/Enum.cs
@@ -1,10 +1,20 @@
 ﻿namespace LaYumba.Functional
 {
-   using static F;
-   
-   public static class Enum
-   { 
-      public static Option<T> Parse<T>(this string s) where T : struct
-         => System.Enum.TryParse(s, out T t) ? Some(t) : None ;
-   }
+    using System.Collections.Generic;
+    using System.Linq;
+    using static F;
+
+    public static class Enum
+    {
+        public static bool TryParse<T>(this string s, out T t)
+        {
+            IEnumerable<T> all = System.Enum.GetValues(typeof(T)).Cast<T>();
+            Dictionary<string, T> sensitiveNames = all.ToDictionary(k => System.Enum.GetName(typeof(T), k));
+
+            return sensitiveNames.TryGetValue(s, out t);
+        }
+
+        public static Option<T> Parse<T>(this string s) where T : struct
+           => Enum.TryParse(s, out T t) ? Some(t) : None;
+    }
 }
